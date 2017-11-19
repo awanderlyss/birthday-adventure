32da16a4317daf4a48d7a05bcb02bb7f2259ff77
awanderlyss <alyssa.felix7@gmail.com>
2017-11-19 05:19:08 -0500

    Completed date page, updated localstorage order

--- resources/js/main.js
+++ resources/js/main.js
@@ -10,6 +10,10 @@ function next_date_func(){
   location.href = "date.html";
 }
 
+function next_bike_func(){
+  location.href = "bike.html";
+}
+
 function light_func(){
   var answer = document.getElementById('light-ans').value.toLowerCase();
   switch (answer) {
@@ -32,24 +36,42 @@ function light_func(){
 function location_func(){
   var lat = document.getElementById('latLocAns').value;
   var long = document.getElementById('longLocAns').value;
-
   if ((lat == '40.6909n') || (lat == '40.6909N') || (lat == '40.6909 N') || (lat == '40.6909 n')) {
     if ((long == '74.9216w') || (long == '74.9216W') || (long == '74.9216 W') || (long == '74.9216 w')) {
       document.getElementById('latLocDiv').innerHTML = '';
       document.getElementById('longLocDiv').innerHTML = '<p class="info-text" style="padding:1rem 0 2rem">You have successfully found the coordinates to the approximate location of your adventure! This clue has been saved to the <span class="clue-text">Clue Bank</span>!</p><button id="light-btn" onclick="next_date_func()" class="hollow button alert info-btn">Next Clue <i class="fa fa-bicycle" aria-hidden="true"></i></button>';
+      localStorage.setItem('lat-answer', lat);
       localStorage.setItem('long-answer', long);
+      var retrievedObject = localStorage.getItem('lat-answer');
+      document.getElementById('clueBank').append(retrievedObject);
       var retrievedObject = localStorage.getItem('long-answer');
       document.getElementById('clueBank').append(retrievedObject);
     } else{
       document.getElementById('longLocAns').value = '';
     }
-    localStorage.setItem('lat-answer', lat);
-    var retrievedObject = localStorage.getItem('lat-answer');
-    document.getElementById('clueBank').append(retrievedObject);
+
   } else{
     document.getElementById('latLocAns').value = '';
   }
+}
 
-
-
+function date_func(){
+  var startDate = document.getElementById('startDateAns').value;
+  var endDate = document.getElementById('endDateAns').value;
+  if ((startDate == '12/8') || (startDate == '12/08') || (startDate == 'December 8') || (startDate == 'December 8th') || (startDate == 'Dec 8') || (startDate == 'Dec 8th')) {
+    if ((endDate == '12/10') || (endDate == 'December 10th') || (endDate == 'December 10') || (endDate == 'Dec 10th') || (endDate == 'Dec 10')) {
+      document.getElementById('startDateDiv').innerHTML = '';
+      document.getElementById('endDateDiv').innerHTML = '<p class="info-text" style="padding:1rem 0 2rem">You have successfully found the coordinates to the approximate location of your adventure! This clue has been saved to the <span class="clue-text">Clue Bank</span>!</p><button id="light-btn" onclick="next_bike_func()" class="hollow button alert info-btn">Next Clue <i class="fa fa-bicycle" aria-hidden="true"></i></button>';
+      localStorage.setItem('startDate-answer', startDate);
+      localStorage.setItem('endDate-answer', endDate);
+      var retrievedObject = localStorage.getItem('startDate-answer');
+      document.getElementById('clueBank').append(retrievedObject);
+      var retrievedObject = localStorage.getItem('endDate-answer');
+      document.getElementById('clueBank').append(retrievedObject);
+    } else{
+      document.getElementById('endDateAns').value = '';
+    }
+  } else{
+    document.getElementById('startDateAns').value = '';
+  }
 }
